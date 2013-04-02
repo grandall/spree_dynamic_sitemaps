a9504c3445f4cca787b74f53b5ccce52b8715781
Graham Randall <grandall@normalmodes.com>
2013-04-01 21:58:04 -0700

    Upgraded for SKU-specific urls.

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -33,43 +33,58 @@ class SitemapController < Spree::BaseController
             end
           end
         end
-        Spree::Product.active.find_in_batches do |group|
-          group.each do |product|
-            v = _build_product_hash(product)
-            xml.url {
-              xml.loc public_dir + v['link']
-              xml.lastmod v['updated'].xmlschema			  #change timestamp of last modified
-              xml.changefreq 'weekly'
-              xml.priority '0.8'
-            } 
+        #Spree::Product.active.find_in_batches do |group|
+        #  group.each do |product|
+        #    v = _build_product_hash(product)
+        #    xml.url {
+        #      xml.loc public_dir + v['link']
+        #      xml.lastmod v['updated'].xmlschema			  #change timestamp of last modified
+        #      xml.changefreq 'weekly'
+        #      xml.priority '0.8'
+        #    } 
+        #  end
+        #end
+        Spree::Product.active.includes(:variants, :manufacturers).find_in_batches do |batch|
+          batch.each do |product|
+            product.variants.each do |variant|
+              next unless variant.active?
+              next unless variant.manufacturer_number.present?
+              v = _build_variant_hash(product, variant)
+              xml.url {
+                xml.loc public_dir + v['link']
+                xml.lastmod v['updated'].xmlschema			  #change timestamp of last modified
+                xml.changefreq 'weekly'
+                xml.priority '0.8'
+              }
+            end
           end
         end
         
-        ActiveRecord::Base.connection.select_all(<<-END
-          select distinct replace(t.permalink, 'manufacturers/','skus/') as permalink, v.manufacturer_number, p.updated_at
-          from spree_products p
-            inner join spree_products_taxons pt
-              on pt.product_id = p.id
-            inner join spree_taxons t 
-              on t.id = pt.taxon_id
-            inner join spree_taxonomies tn 
-              on tn.id = t.taxonomy_id
-                and tn.name = 'Manufacturers'
-            inner join spree_variants v 
-              on v.product_id = p.id
-                and v.deleted_at is null
-                and p.deleted_at is null
-                and v.is_master = 0
-                and v.manufacturer_number is not null;
-        END
-        ).each do |row|
-          xml.url {
-            xml.loc public_dir + row['permalink'] + "/#{CGI.escape(row['manufacturer_number'])}"
-            xml.lastmod row['updated_at'].xmlschema
-            xml.changefreq 'weekly'
-            xml.priority '0.8'
-          }
-        end
+        #ActiveRecord::Base.connection.select_all(<<-END
+        #  select distinct replace(t.permalink, 'manufacturers/','skus/') as permalink, v.manufacturer_number, p.updated_at
+        #  from spree_products p
+        #    inner join spree_products_taxons pt
+        #      on pt.product_id = p.id
+        #    inner join spree_taxons t 
+        #      on t.id = pt.taxon_id
+        #    inner join spree_taxonomies tn 
+        #      on tn.id = t.taxonomy_id
+        #        and tn.name = 'Manufacturers'
+        #    inner join spree_variants v 
+        #      on v.product_id = p.id
+        #        and v.deleted_at is null
+        #        and p.deleted_at is null
+        #        and v.is_master = 0
+        #        and v.manufacturer_number is not null;
+        #END
+        #).each do |row|
+        #  xml.url {
+        #    xml.loc public_dir + row['permalink'] + "/#{CGI.escape(row['manufacturer_number'])}"
+        #    xml.lastmod row['updated_at'].xmlschema
+        #    xml.changefreq 'weekly'
+        #    xml.priority '0.8'
+        #  }
+        #end
         
       }
     end
@@ -90,6 +105,15 @@ class SitemapController < Spree::BaseController
     pinfo['link'] = 'products/' + product.permalink	# primary
     pinfo['updated'] = product.updated_at
     pinfo
+  end
+
+  def _build_variant_hash(variant)
+    product = variant.product
+    vinfo = {}
+    vinfo['name'] = product.name + ' (' + variant.manufacturer_number + ')'
+    vinfo['link'] = ['skus', product.manufacturer.permalink.split('/').last, variant.manufacturer_number].join('/')
+    vinfo['updated'] = product.updated_at
+    vinfo
   end
   
 end
